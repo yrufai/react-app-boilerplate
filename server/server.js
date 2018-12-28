a78662cb289ec51e313e14cd0a81c0622fa18b3f
yakubu rufai mahatma <yrufai23@gmail.com>
2018-12-28 12:24:13 +0000

    server setup

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 5050;
+const publicPath = path.join(__dirname, "..", "public");
+
+app.use("*", (req, res)=>{
+    res.sendFile(path.join(publicPath, "index.html"))
+});
+
+app.listen(port, ()=>{
+    console.log("server is working ")
+})
