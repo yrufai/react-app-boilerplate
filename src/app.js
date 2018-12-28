e154058007728db6639c9f4a5f33493b81ffc851
yakubu rufai mahatma <yrufai23@gmail.com>
2018-12-28 15:27:44 +0000

    app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import Home from "./components/Home"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/style.scss";
-
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fab);
 
 ReactDOM.render(<Home />, document.getElementById("app")); 
